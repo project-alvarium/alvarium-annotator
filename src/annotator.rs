d609c42e38eae737f03a966b229ea67f6888ecdd
DyrellC <dyrell@demia.net>
2024-08-22 15:41:48 -0600

    Annotator implementing Send + Sync

--- src/annotator.rs
+++ src/annotator.rs
@@ -1,6 +1,6 @@
 use crate::Annotation;
 
-pub trait Annotator {
+pub trait Annotator: Send + Sync {
     type Error: std::error::Error;
     fn execute(&mut self, data: &[u8]) -> Result<Annotation, Self::Error>;
 }
