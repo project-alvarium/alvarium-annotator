d13543cf3cbe4fa228f6ae6673372b70bdfbe0de
Brord van Wierst <brord@brord.net>
2024-11-11 20:51:18 +0100

    added missing trait

--- src/annotator.rs
+++ src/annotator.rs
@@ -1,7 +1,7 @@
 use crate::Annotation;
 
 #[async_trait::async_trait]
-pub trait Annotator {
+pub trait Annotator: Send + Sync {
     type Error: std::error::Error;
     async fn execute(&mut self, data: &[u8]) -> Result<Annotation, Self::Error>;
 }
